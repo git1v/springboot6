53c9f5aa6c792eef6291263ae974b9144445eaa6
dean <git1v@yahoo.co.jp>
2017-07-10 16:16:46 +0900

    use simple tag name to get inputData

--- src/main/java/com/rose/web/QuestionController.java
+++ src/main/java/com/rose/web/QuestionController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,20 +19,28 @@ public class QuestionController {
 	public ModelAndView index(ModelAndView mav){
 		mav.addObject("title", "EnglishPractice");
 
-		ArrayList<QuestionData> data=new ArrayList<>();
 		ArrayList<Integer> ids=new ArrayList<>();
 		ids.add(new Integer(1));
 		ids.add(new Integer(2));
 		ids.add(new Integer(3));
 		ids.add(new Integer(4));
-		for(Integer id : ids){
-			data.add(repository.findById((Integer)id));
+		
+		for(int i=0;i<ids.size();i++){
+			QuestionData data=repository.findById((Integer)ids.get(i));
+			String dataName="data"+(i+1);
+			mav.addObject(dataName,data);
 		}
-		mav.addObject("data",data);
 		mav.setViewName("index");
 		return mav;
 	}
-	
 
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(
+			@RequestParam("input1")String input1
+			,ModelAndView mav){
+		mav.addObject("title", input1);
+		mav.setViewName("index");
+		return mav;
+	}
 }
 
