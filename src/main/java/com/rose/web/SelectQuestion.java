44fa556cad18ec066f30739a3935f9771a083b18
dean <git1v@yahoo.co.jp>
2017-07-15 00:50:35 +0900

    made OKList and NGList according to rate of anslog

--- src/main/java/com/rose/web/SelectQuestion.java
+++ src/main/java/com/rose/web/SelectQuestion.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ public class SelectQuestion {
 	@Autowired
 	AnswerLogEntityRepository anslogRepository;
 	int studentid=101;
+	final double OKNG_VRITERION=(double)0.8;
 	public ArrayList<Integer> select(int COUNT_OF_QUESTION){
 		ArrayList<Integer> allIds=new ArrayList<>();
 
@@ -27,7 +29,7 @@ public class SelectQuestion {
 		System.out.println("userlist size= "+userlist.size());
 		
 		//student_idを指定してanslogテーブルから解答状況を取得する
-		HashMap done=new HashMap();
+		HashMap<Integer, AnswerStatusBean> done=new HashMap<>();
 		
 		List<AnswerLogEntity> anslogList=anslogRepository.findByStudentid((Integer)studentid);
 		for(AnswerLogEntity anslog:anslogList){
@@ -53,11 +55,21 @@ public class SelectQuestion {
 		}
 		System.out.println("既に解答した設問数= "+done.size());
 		
-		
+		//基準に応じてOK問題とNG問題に分ける
+		HashMap<Integer, AnswerStatusBean> OKMap=new HashMap<>();
+		HashMap<Integer, AnswerStatusBean> NGMap=new HashMap<>();
+		for (Entry<Integer, AnswerStatusBean> entry : done.entrySet()) {
+            double rate = (double)entry.getValue().getRate();
+            if(rate<OKNG_VRITERION){
+            	OKMap.put((Integer)entry.getKey(), entry.getValue());
+            }else{
+            	NGMap.put((Integer)entry.getKey(), entry.getValue());
+            }
+        }
 		List<StageLog>stageloglist=stagelogRepository.findByStudentid((Integer)studentid);
 		System.out.println("student stage= "+stageloglist.get(0).getStage());
-		
-		
+		System.out.println("OK設問数= "+OKMap.size());
+		System.out.println("NG設問数= "+NGMap.size());
 		
 		
 		for(int index=0;index<data.size();index++){
