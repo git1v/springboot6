0bdbecb5b7a79177b00697eaf25e8df9e7280f33
dean <git1v@yahoo.co.jp>
2017-07-15 06:05:33 +0900

    response to the rate of anslog

--- src/main/java/com/rose/web/QuestionRepository.java
+++ src/main/java/com/rose/web/QuestionRepository.java
@@ -4,10 +4,20 @@ import java.util.List;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 @ComponentScan 
 @Repository
 public interface QuestionRepository extends JpaRepository<QuestionData, Integer> {
 	public QuestionData findById(Integer id);
-	public List<QuestionData> findAll();
+	public List<QuestionData> findByStageBetween(int start, int end);
+	
+	public static final String FIND_ID="select id from qlist";
+	@Query(value=FIND_ID, nativeQuery=true)
+	public List<Integer>findOnlyIds();
+	
+	public static final String FIND_ID_BETWEEN="select id from qlist where stage between ?1 and ?2";
+	@Query(value=FIND_ID_BETWEEN, nativeQuery=true)
+	public List<Integer>findIdsBetween(int start, int end);
+	
 }
